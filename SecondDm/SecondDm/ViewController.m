4d61414e9f6290b9a050e9ddc9fe069b50b518c9
oxweiwei <student@ZXW.local>
2016-06-01 11:26:10 +0800

    oxweiwei
    hebing

---
+++ SecondDm/SecondDm/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SecondDm
+//
+//  Created by student on 16/6/1.
+//  Copyright © 2016年 ZXW. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"请求合并");
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
